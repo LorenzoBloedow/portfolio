25c496f79fc7e5fcf1944bb87437ebec07dfc598
Lorenzo Bloedow <lorenzobloedow@gmail.com>
2023-07-17 15:20:14 -0700

    Create Website Intro

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,24 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+        screens: {
+            xs: '318px',
+            sm: '373px',
+            md: '766px',
+            lg: '1278px',
+            xl: '1918px'
+        },
+		extend: {
+            fontFamily: {
+                climate: ["Climate Crisis", "sans-serif"],
+                sans: ["Cabin", "sans-serif"]
+            },
+            colors: {
+                "primary": "var(--primary)",
+                "secondary": "var(--secondary)",
+                "accent": "var(--accent)"
+            }
+        }
 	},
 	plugins: [],
     future: {
