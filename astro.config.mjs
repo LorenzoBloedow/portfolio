3383d7c78c4ad3badc62506605cd5b26ca674b66
Lorenzo Bloedow <lorenzobloedow@gmail.com>
2023-07-23 12:55:08 -0700

    Create Vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel({
+    analytics: true
+  })
 });
